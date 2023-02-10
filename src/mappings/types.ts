308b8b9b22f6e641f0f670864ec7c4d3d4bcf593
chezhe <chezhe@hey.com>
2023-02-10 15:39:40 +0800

    migrate from testnet repo

--- src/mappings/types.ts
+++ src/mappings/types.ts
@@ -1,8 +1,24 @@
 import { SubstrateExtrinsic, SubstrateBlock } from "@subql/types";
+import { Balance } from '@polkadot/types/interfaces'
 import type { CallBase, AnyTuple } from '@polkadot/types/types'
 import { Extrinsic, EventRecord, SignedBlock } from '@polkadot/types/interfaces';
 
 export type AnyCall = CallBase<AnyTuple>
+
+// Type construct is not correct
+export type AccountType = {
+  nonce: number
+  consumers: number
+  providers: number
+  sufficients: number
+  data: {
+    free: Balance
+    reserved: Balance
+    miscFrozen: Balance
+    feeFrozen: Balance
+  }
+}
+
 
 export interface WrappedExtrinsic {
   idx: number;
@@ -11,3 +27,5 @@ export interface WrappedExtrinsic {
   events: EventRecord[];
   success: boolean;
 }
+
+export type AccountIdMap = { [key: string]: null | string }
